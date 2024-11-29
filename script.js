3d425f166f1ffe1b2b67b976908a30d2951fc950
hasnain501 <haxnain433@gmail.com>
2024-11-29 23:18:10 +0500

    dmt

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Simple JavaScript to display an alert when the form is submitted
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.');
+});
